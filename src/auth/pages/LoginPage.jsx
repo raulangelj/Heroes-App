ed14277c4e04219653cefb78b61774346672ffe6
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-10 23:05:54 -0600

    chore: add login and logout simple functionality

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context';
 
 export const LoginPage = () => {
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext)
 
   const onLogin = () => {
+    login('Raul Angel');
     navigate('/marvel', { replace: true });
   }
 
