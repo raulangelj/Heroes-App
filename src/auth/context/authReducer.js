411a13ec7b223211775139de00ab2ea6ec72fb6a
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-10 22:38:11 -0600

    chore: add auth context

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,19 @@
+import { types } from "../types";
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case types.login:
+      return {
+        ...state,
+        logged: true,
+        name: action.payload,
+      };
+    case types.logout:
+      return {
+        ...state,
+        logged: false,
+      };
+    default:
+      return state;
+  }
+};
